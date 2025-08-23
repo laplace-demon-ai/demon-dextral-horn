52eb1ba47449b05df189ad97919dc549a4c25742
moe-mizrak <mhmmdmzrk@gmail.com>
2025-08-24 00:35:56 +0300

    test: some tests are skipped for now

--- tests/Resolvers/RouteParamResolverTest.php
+++ tests/Resolvers/RouteParamResolverTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\Resolvers;
 
 use Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
 /**
  * Test for RouteParamResolver.
@@ -16,5 +17,11 @@ final class RouteParamResolverTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
+    }
+
+    #[Test]
+    public function it_tests(): void
+    {
+        $this->markTestSkipped('This test is not ready at the moment, skipping.');
     }
 }
